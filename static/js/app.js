3363ebd3b5296b085d684e724ead4964a6119de4
Steve Walker <bsteven.walker@gmail.com>
2021-07-26 08:57:46 -0400

    map and radio buttons are done

--- static/js/app.js
+++ static/js/app.js
@@ -38,9 +38,9 @@ var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/
 ////////////////////////////////////////////////////
 // Define a baseMaps object to hold our base layers
 var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap,
-    "Grayscale Map": grayscalemap
+    "Grayscale": grayscalemap
+    ,"Street": streetmap
+    ,"Dark": darkmap
 };
 
 
@@ -53,32 +53,49 @@ var overlayMaps = {
 
 
 ////////////////////////////////////////////////////
+// Create Map
+var myMap = L.map("map", {
+    center: [34.0522, -118.357],
+    zoom: 9.5,
+    layers: [grayscalemap
+            , dispensaries
+            , crime] //ORDERING THIS SO THAT THE TOOLTIPS WORK
+});
+
+////////////////////////////////////////////////////
+// Create a layer control
+// Pass in our baseMaps and overlayMaps
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+
+////////////////////////////////////////////////////
 // Map disp data
-//Main function to call child functions
-function optionChanged(yearVal) {
+function createDisp(yearVal) {
 
     d3.json(dispQuery).then(function (data) {
         // console.log(data.features)
-        
-        var y = yearVal
+
+        // Clear layer on val change
+        dispensaries.clearLayers();
+
         // Define a function we want to run once for each feature in the features array
         function onEachFeature(feature, layer) {
             layer.bindPopup("<h3>" + feature.properties.business_name +
-                "</h3><hr><p>" + new Date(feature.properties.location_start_date) + "</p>");
+                "</h3><hr><p>" + new Date(feature.properties.location_start_date) + 
+                "</p><p>" + feature.properties.street_address + "</p>");
         }
 
+        // Create filter for year
         function yearFilter(feature) {
-            if (feature.properties.location_start_year < 2010) return true
+            if (feature.properties.location_start_year <= yearVal) return true
         }
-
-        // var listDisp = L.geoJSON(data, {filter: yearFilter});
-        // console.log(listDisp)
 
         // Create a GeoJSON layer containing the features array on the earthquakeData object
         // Add GeoJSON to the earthquakes layergroup
         L.geoJSON(data.features, {
             onEachFeature: onEachFeature
-            ,filter: yearFilter
+            , filter: yearFilter
         }).addTo(dispensaries);
     });
 
@@ -87,112 +104,157 @@ function optionChanged(yearVal) {
 
 //////////////////////////////////////////////////
 // Map crime data
-d3.json(crimesQuery).then(function (data) {
-    // Creating style for the choropleth
+function createCrime(yearVal) {
+    d3.json(crimesQuery).then(function (data) {
+        // Clear layer on val change
+        crime.clearLayers();
 
-    function style(feature) {
-        return {
-            fillColor: getColor(feature.properties.crime_counts),
-            weight: 2,
-            opacity: 1,
-            color: 'white',
-            dashArray: '3',
-            fillOpacity: 0.7
+        // Creating style for the choropleth
+        function style(feature) {
+            return {
+                fillColor: getColor(feature.properties.crime_counts),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7
+            }
         }
-    }
 
-    // Add crimes GeoJSON to the techtonics layergroup
-    L.geoJSON(data.features, { style: style }).addTo(crime)
+        function filter(feature) {
+            if (feature.properties.year == yearVal) return true
+        }
 
-});
+        // Add crimes GeoJSON to the techtonics layergroup
+        L.geoJSON(data.features, { 
+            style: style
+            , filter: filter 
+        }).addTo(crime)
 
-// Create Map
-var myMap = L.map("map", {
-    center: [34.0522, -118.30],
-    zoom: 11,
-    layers: [darkmap, dispensaries, crime] //ORDERING THIS SO THAT THE TOOLTIPS WORK
-});
+    });
+}
 
-// Create a layer control
-// Pass in our baseMaps and overlayMaps
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-}).addTo(myMap);
-
-
+//////////////////////////////////////////////////
+// Define colors for choropleth
 function getColor(counts) {
     var color;
 
-    if (counts < 6000) { color = '#33FF61' }
-    else if (counts < 6500) { color = '#DDFF33' }
-    else if (counts < 7000) { color = '#FFE333' }
-    else if (counts < 7500) { color = '#E6b52E' }
-    else if (counts < 8000) { color = '#CC9329' }
-    else { color = '#A35322' }
+    if (counts < 5000) { color = '#FECFCF' }
+    else if (counts < 5500) { color = '#FE9F9F' }
+    else if (counts < 6000) { color = '#FD504F'  }
+    else if (counts < 6500) { color = '#FD0100' }
+    else if (counts < 7000) { color = '#b01030' }
+    else if (counts < 7500) { color = '#9a0e2a' }
+    else if (counts < 8000) { color = '#840c24'}
+    else { color = '#6e0a1e' }
 
     return color;
 }
 
 
+//////////////////////////////////////////////////
+// LEGEND
+// Create a legend to display information about our map
+var legend = L.control({ position: "bottomright" });
+
+// When the layer control is added, insert a div with the class of "legend"
+legend.onAdd = function () {
+    var div = L.DomUtil.create("div", "legend");
+    categories = ['-10—9', '10—29', '30—49', '50—69', '70—89', '90+'];
+
+    div.innerHTML += '';
+    for (var i = 0; i < categories.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(categories[i]) + '"></i> ' +
+            (categories[i] ? categories[i] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
 
 ////////////////////////////////////////////////////
-// // LEGEND
-// // Create a legend to display information about our map
-// var legend = L.control({ position: "bottomright" });
-
-// // When the layer control is added, insert a div with the class of "legend"
-// legend.onAdd = function () {
-//     var div = L.DomUtil.create("div", "legend");
-//     categories = ['-10—9', '10—29', '30—49', '50—69', '70—89', '90+'];
-
-//     div.innerHTML += '';
-//     for (var i = 0; i < categories.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + getColor(categories[i]) + '"></i> ' +
-//             (categories[i] ? categories[i] + '<br>' : '+');
-//     }
-
-//     return div;
-// };
-
-// legend.addTo(myMap);
-
-////////////////////////////////////////////////////////////////////////
 //Populates the dropdown list
 function populateDropDown() {
-    var dropdownTag = document.getElementById("selDataset");
+    // var dropdownTag = document.getElementById("selDataset");
 
-    var year = [2013, 2014, 2015, 2016, 2017, 2018, 2019];
-  
+    // var year = [2019, 2018, 2017, 2016, 2015, 2014, 2013];
+
+    // for (var i = 0; i < year.length; i++) {
+    //     var newOption = year[i];
+
+    //     var el = document.createElement("option");
+    //     el.textContent = newOption;
+    //     el.value = newOption;
+
+    //     dropdownTag.append(el);
+    // }
+
+    var radioTag = document.getElementById("control");
+
+    var year = [2019, 2018, 2017, 2016, 2015, 2014, 2013];
+
     for (var i = 0; i < year.length; i++) {
-      var newOption = year[i];
-  
-      var el = document.createElement("option");
-      el.textContent = newOption;
-      el.value = newOption;
-  
-      dropdownTag.append(el);
+        var newRadio = year[i];
+
+        // Create inputs for label
+        var inp =document.createElement("input")
+        inp.type = "radio";
+        inp.id = newRadio;
+        inp.name = "year";
+        inp.value = newRadio;
+        inp.setAttribute("onchange","optionChanged(this.value)");
+
+        // Add a default checked box to the first option selected
+        if (i==0){inp.setAttribute("checked","checked")}
+
+        // Add new radio to control tag
+        radioTag.append(inp);
+
+        // Create label for the radio
+        var lb = document.createElement("label")
+        lb.setAttribute("for", newRadio)
+        lb.textContent = newRadio
+
+        // Add input to the control tag
+        radioTag.append(lb);    
     }
-  };
+    
+};
 
 
-////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 //Initializes the Dashboard
-function init(){
+function init() {
     var val = 2019;
 
     populateDropDown();
     optionChanged(val);
+
+
     // d3.json(samples).then((data) => {
     //   var listDDVals = data.names; //filter the json by getting just the names tag info
-    
+
     //   populateDropDown(listDDVals);
-    
+
     //   const defaultVal = listDDVals[0];
     //   //console.log(defaultVal); //sanity check
-  
+
     //   //optionChanged(defaultVal)
     // });
-  
-  }
+}
+
+
+////////////////////////////////////////////////////
+//Captures on change values
+function optionChanged(val) {
+    console.log(val);
+    createDisp(val);
+    createCrime(val);
+}
+
+
+function autoRun(
+    
+)
